c04e462e7fdbd820dc4c0130e3bd654b3a4df35f
董文賓 <a30082424@gmail.com>
2024-09-19 22:40:31 +0800

    version0919-2

---
+++ js/cookie.js
@@ -0,0 +1,14 @@
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        let date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+$('#add').click(function(){
+
+    var name = $('#name').val();
+    setCookie('username', name, 999999);
+})
